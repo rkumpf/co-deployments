ec0a01658d4ec1198758f394f93e326070bf7e5f
Roger Kumpf <roger.kumpf@hpe.com>
2021-01-13 14:18:26 -0800

    Include subresources status in the CRD as part of kubebuilder generation
    Update the hpecsidriver_types.go file so that the kubebuilder "make manifests"
    results can be used without modification.  Also generate the CRD using the v1
    API instead of v1beta1.
    
    Jira: CON-1678

--- operators/hpe-csi-operator/deploy/generated/api/v1/hpecsidriver_types.go
+++ operators/hpe-csi-operator/deploy/generated/api/v1/hpecsidriver_types.go
@@ -23,6 +23,7 @@ import (
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // HPECSIDriverSpec defines the desired state of HPECSIDriver
+// +kubebuilder:subresource:status
 type HPECSIDriverSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
